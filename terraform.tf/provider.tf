fdd3a6aa3d748c41ab4ca0ace1e3fbf2f9fda478
suresh <sureshjala@7gmail.com>
2022-10-14 19:51:26 +0530

    removed keys

--- terraform.tf/provider.tf
+++ terraform.tf/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-west-2"
-  access_key = "AKIATSIRBGO3PAITSOQR"
-  secret_key = "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"
+  access_key = "*******"
+  secret_key = "*******"
 }
